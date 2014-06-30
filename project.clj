0471929eb283575f2a6bc4f1199e6ba8c0779142
xensky <xensky@gmail.com>
2014-06-29 21:03:40 -0400

    add main to project so app can run

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "project alchemy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main alchemy.core)
